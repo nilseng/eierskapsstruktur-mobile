2e93ff135c728c407d312909494a84627ff5de77
Teodor Nilseng Danielsen <teodor.nilseng@gmail.com>
2021-08-18 00:09:08 +0200

    applying theme in CompanyDetails component

--- components/CompanyDetails.tsx
+++ components/CompanyDetails.tsx
@@ -1,18 +1,19 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {GlobalContext} from '../App';
 
 export const CompanyDetails = () => {
   const {
     routeContext: {params: company},
+    theme,
   } = useContext(GlobalContext);
 
   return (
     <View style={styles.container}>
       {company && (
         <>
-          <Text>{company.name}</Text>
-          <Text>Stocks: {company.stocks}</Text>
+          <Text style={{color: theme.text}}>{company.name}</Text>
+          <Text style={{color: theme.text}}>Stocks: {company.stocks}</Text>
         </>
       )}
     </View>
